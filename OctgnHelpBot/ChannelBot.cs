4a3753d12e1e7401044d45b75c0c7fa3ca697503
KellyElton <kelly.elton@skylabsonline.com>
2013-03-22 12:45:40 -0500

    First Commit

---
+++ OctgnHelpBot/ChannelBot.cs
@@ -0,0 +1,140 @@
+﻿namespace TrollBot
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Timers;
+    using System.Web;
+
+    using ChatterBotAPI;
+
+    using IrcDotNet;
+
+    public class ChannelBot
+    {
+        internal IrcChannel Channel { get; set; }
+        internal bool Silence { get; set; }
+        internal bool dboResponded { get; set; }
+        internal ChatterBotSession Bot { get; set; }
+        internal string Who = "Db0";
+        private Random random = new Random();
+        internal int noDb0Counter = 10;
+
+        public ChannelBot(IrcChannel channel)
+        {
+            Channel = channel;
+
+            Channel.UsersListReceived += ChannelOnUsersListReceived;
+            Channel.UserJoined += ChannelOnUserJoined;
+            Channel.UserLeft += ChannelOnUserLeft;
+            Channel.MessageReceived += ChannelOnMessageReceived;
+            var b = new ChatterBotAPI.ChatterBotFactory().Create(ChatterBotType.CLEVERBOT);
+            Bot = b.CreateSession();
+            //HelloTimerOnElapsed(null, null);
+        }
+
+        internal void Message(string message)
+        {
+            const string template = "PRIVMSG {0} :{1}";
+            Channel.Client.SendRawMessage(String.Format(template, Channel.Name, message));
+        }
+
+        private void ChannelOnUsersListReceived(object sender, EventArgs eventArgs)
+        {
+            //Message("Hello!");
+        }
+
+        private void ChannelOnMessageReceived(object sender, IrcMessageEventArgs ircMessageEventArgs)
+        {
+            if (ircMessageEventArgs.Source.Name == "GaryBot") return;
+            //if (ircMessageEventArgs.Text.Trim().EndsWith("?"))
+            //{
+            //    this.Message("http://lmgtfy.com/?q=" + HttpUtility.UrlEncode(ircMessageEventArgs.Text));
+            //}
+            return;
+            var from = ircMessageEventArgs.Source.Name.ToLower();
+            var num = random.Next(0, 100);
+            if (from.Contains(Who.ToLower()) || from.Contains("brine") || num > 55)
+            {
+                noDb0Counter = 0;
+                dboResponded = true;
+                var reply = "";
+
+                while (String.IsNullOrWhiteSpace(reply))
+                {
+                    try
+                    {
+                        reply = Bot.Think(ircMessageEventArgs.Text);
+                    }
+                    catch (Exception)
+                    {
+                        try
+                        {
+                            Bot = new ChatterBotAPI.ChatterBotFactory().Create(ChatterBotType.CLEVERBOT).CreateSession();
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+
+                this.Message(reply);
+            }
+
+        }
+
+        private void ChannelOnUserLeft(object sender, IrcChannelUserEventArgs ircChannelUserEventArgs)
+        {
+            //Message(ircChannelUserEventArgs.ChannelUser.User.NickName + " was a real dick that year...");
+        }
+
+        private object handleJoinLocker = new object();
+        private bool HandleUserJoin(string username)
+        {
+            lock (handleJoinLocker)
+            {
+                var ret = false;
+                username = username.ToLower();
+                if (!File.Exists("users.txt")) File.Create("users.txt").Close();
+                var userListRaw = File.ReadAllLines("users.txt");
+                var userList = userListRaw.ToDictionary(x => x.Split(' ')[0].ToLower(), y => y.Split(' ')[1]);
+                if (userList.ContainsKey(username))
+                {
+                    var timeSpan = new TimeSpan(DateTime.Now.Ticks - DateTime.Parse(userList[username]).Ticks);
+                    if (timeSpan.Days > 10) ret = true;
+                    userList[username] = DateTime.Now.ToString();
+                    File.WriteAllLines("users.txt", userList.Select(x => x.Key + " " + x.Value));
+                }
+                else
+                {
+                    ret = true;
+                    userList.Add(username,DateTime.Now.ToString());
+                    File.WriteAllLines("users.txt", userList.Select(x => x.Key + " " + x.Value));
+                }
+                return ret;
+            }
+        }
+
+        private void ChannelOnUserJoined(object sender, IrcChannelUserEventArgs ircChannelUserEventArgs)
+        {
+            if (this.HandleUserJoin(ircChannelUserEventArgs.ChannelUser.User.NickName))
+            {
+                this.Message(
+                    "Hello, " + ircChannelUserEventArgs.ChannelUser.User.NickName
+                    + ". If you need help may I recommend http://www.octgn.net/help ?");
+                this.Message(
+                    "If you need to report an issue you can always do that here https://github.com/kellyelton/OCTGN/issues .");
+                this.Message("If you are feeling patient, please, stick around and someone will be with you shortly.");
+            }
+            else
+            {
+                
+            }
+            //this.Message("Hello " + ircChannelUserEventArgs.ChannelUser.User.NickName);
+        }
+    }
+}
---
+++ OctgnHelpBot/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace TrollBot
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using IrcDotNet;
+
+    internal class Program
+    {
+        private static bool closingTime;
+
+        internal static IrcClient client;
+
+        private static IrcClient octgnClient;
+        private static IrcClient octgnDevClient;
+        private static List<ChannelBot> channels = new List<ChannelBot>();
+        private static bool gotChannels;
+        static void Main(string[] args)
+        {
+            client = new IrcClient();
+            var reg = new IrcUserRegistrationInfo();
+            reg.NickName = "GaryBot";
+            reg.UserName = "GaryBot";
+            reg.RealName = "GaryBot";
+            client.Disconnected += ClientOnDisconnected;
+            client.Connected += ClientOnConnected;
+            client.ErrorMessageReceived += ClientOnErrorMessageReceived;
+            client.MotdReceived += ClientOnMotdReceived;
+            client.RawMessageReceived += ClientOnRawMessageReceived;
+            client.ChannelListReceived += ClientOnChannelListReceived;
+            client.ProtocolError += ClientOnProtocolError;
+            client.RawMessageSent += ClientOnRawMessageSent;
+            client.Error += client_Error;
+            client.Connect("irc.freenode.net",6667,false,reg);
+            while (!closingTime)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        static void client_Error(object sender, IrcErrorEventArgs e)
+        {
+            Log("Client Error: " + e.Error.Message, ConsoleColor.Magenta);
+        }
+
+        private static void ClientOnRawMessageSent(object sender, IrcRawMessageEventArgs ircRawMessageEventArgs)
+        {
+            if(ircRawMessageEventArgs != null)
+                Log("Message Sent: " + ircRawMessageEventArgs.RawContent, ConsoleColor.DarkGreen);
+        }
+
+        private static void Connect()
+        {
+            
+        }
+
+        private static void ClientOnProtocolError(object sender, IrcProtocolErrorEventArgs ircProtocolErrorEventArgs)
+        {
+            Log("Error: " + ircProtocolErrorEventArgs.Message, ConsoleColor.Magenta);
+            if (ircProtocolErrorEventArgs.Code == 433)
+            {
+                
+            }
+        }
+
+        private static void ClientOnChannelListReceived(object sender, IrcChannelListReceivedEventArgs ircChannelListReceivedEventArgs)
+        {
+            Log("Got those channels yo");
+            gotChannels = true;
+            foreach (var c in ircChannelListReceivedEventArgs.Channels)
+            {
+                Log("Trying to join channel " + c.Name);
+                var tc = c;
+                client.Channels.Join(c.Name);
+                var task = new Task(
+                    () =>
+                        {
+                            while (client.Channels.FirstOrDefault(x => x.Name == tc.Name) == null)
+                            {
+                                Thread.Sleep(10);
+                            }
+                            Log("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+;                            channels.Add(new ChannelBot(client.Channels.First(x => x.Name == tc.Name)));
+                        });
+                task.Start();
+            }
+        }
+
+        private static void ClientOnRawMessageReceived(object sender, IrcRawMessageEventArgs ircRawMessageEventArgs)
+        {
+            Log(ircRawMessageEventArgs.RawContent, ConsoleColor.White);
+            try
+            {
+                var mess = JsonConvert.SerializeObject(ircRawMessageEventArgs.Message);
+                Log(mess);
+            }
+            catch
+            {
+
+            }
+            if (ircRawMessageEventArgs.Message.Parameters[0] == "gary199567" && ircRawMessageEventArgs.Message.Source.Name == "kellyelton")
+            {
+                client.SendRawMessage("PRIVMSG #octgn :" + ircRawMessageEventArgs.Message.Parameters[1]);
+            }
+            
+        }
+
+        private static void ClientOnMotdReceived(object sender, EventArgs eventArgs)
+        {
+            Log("motd");
+            new Task(() =>
+                         {
+                             while (!gotChannels)
+                             {
+                                 Log("Getting chanel list");
+                                 //client.Channels.Join("#octgn");
+                                 //client.SendRawMessage("LIST #octgn");
+                                 client.ListChannels("#octgn");
+                                 Thread.Sleep(20000);
+                             }
+                         }).Start();
+            //channels.Add(new ChannelBot(client.Channels.First(x => x.Name == "#octgn")));
+            //channels.Add(new ChannelBot(client.Channels.First(x => x.Name == "#octgn-dev")));
+        }
+
+        private static void ClientOnErrorMessageReceived(object sender, IrcErrorMessageEventArgs ircErrorMessageEventArgs)
+        {
+            Log("Error: " + ircErrorMessageEventArgs.Message);
+        }
+
+        private static void ClientOnConnected(object sender, EventArgs eventArgs)
+        {
+            Log("Connected");
+            //client.ListChannels("octgn","octgn-dev");
+            //client.Channels.Join("octgn", "octgn-dev");
+        }
+
+        private static void ClientOnDisconnected(object sender, EventArgs eventArgs)
+        {
+            Log("Disconnected");
+            closingTime = true;
+        }
+
+        private static object logLock = new object();
+        private static void Log(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            lock (logLock)
+            {
+                var o = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                Console.ForegroundColor = o;
+            }
+        }
+    }
+}
